7ec7301d5f98f54f6adb4e3d6afa09ac801cec8c
weidav <david.weissensteiner@gmail.com>
2021-09-30 15:58:43 +0200

    [tutorial] step3.2

--- lib/main.dart
+++ lib/main.dart
@@ -3,22 +3,41 @@
 // found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Pflanzen & Sprossen'),
         ),
-        body: const Center(
-          child: Text('First Screen'),
+        body: Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
+    );
+  }
+}
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({ Key? key }) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
