4c0604cd14465dced4aa573e91cf7fe6c5193c93
Marvin Ottersberg <marvin.ottersberg@student.fh-kiel.de>
2021-10-06 18:34:52 +0200

    ANF2: GridView auf dritter Route implementiert

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class _FirstRouteState extends State<FirstRoute> {
               },
             ),
             ElevatedButton(
-              child: Text('Open third route'),
+              child: Text('Sprossen'),
               onPressed: () {
                 Navigator.pushNamed(context, '/third');
               },
@@ -70,7 +70,7 @@ class _FirstRouteState extends State<FirstRoute> {
             ),
             ListTile(
               title: const Text('First Route'),
-              leading: Icon(Icons.grass_rounded),
+              leading: Icon(Icons.filter_vintage_rounded),
               onTap: () {
                 //Update the state of the app
                 Navigator.popAndPushNamed(context, '/');
@@ -87,8 +87,8 @@ class _FirstRouteState extends State<FirstRoute> {
               },
             ),
             ListTile(
-              title: const Text('Third Route'),
-              leading: Icon(Icons.filter_vintage_rounded),
+              title: const Text('Sprossen'),
+              leading: Icon(Icons.grass_rounded),
               onTap: () {
                 //Update the state of the app
                 Navigator.popAndPushNamed(context, '/third');
@@ -140,18 +140,113 @@ class _ThirdRouteState extends State<ThirdRoute> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Third Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-            // Navigate back to first route when tapped.
-          },
-          child: Text('Go back!'),
+        appBar: AppBar(
+          title: Text("Sprossen"),
+          backgroundColor: Color.fromRGBO(18, 185, 24, 0.8),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: [
+              /*ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  // Navigate back to first route when tapped.
+                },
+                child: Text('Go back!'),
+              ),*/
+              Expanded(
+                  child: CustomScrollView(
+                primary: false,
+                slivers: <Widget>[
+                  SliverPadding(
+                    padding: const EdgeInsets.all(10),
+                    sliver: SliverGrid.count(
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text("He'd have you all unravel at the"),
+                          color: Colors.green[100],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Heed not the rabble'),
+                          color: Colors.green[200],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Sound of screams but the'),
+                          color: Colors.green[300],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Who scream'),
+                          color: Colors.green[400],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Revolution is coming...'),
+                          color: Colors.green[500],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Revolution, they...'),
+                          color: Colors.green[600],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('Rsdasda'),
+                          color: Colors.green[40],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('sadsady...'),
+                          color: Colors.green[800],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('test'),
+                          color: Colors.green[100],
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          child: const Text('sadsasdasdasd..'),
+                          color: Colors.green[800],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ) /*GridView.count(
+                      crossAxisCount: 2,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: const Text("He'd have you all unravel at the"),
+                      color: Colors.green[100],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      child: const Text("text 2"),
+                      color: Colors.green[300],
+                    ),
+                  ]
+                      List.generate(20, (index) {
+                      return Center(
+                      child: Text(
+                        'Item $index',
+                        style: Theme.of(context).textTheme.headline5,
+                    ),
+                  );
+                }),
+                */
+                  )
+            ],
+          ),
+        ));
   }
 }
