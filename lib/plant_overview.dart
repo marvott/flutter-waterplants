d9e14ca9e828c8bb57e39803f7f5d46f425d7b17
david.weissensteiner <david.weissensteiner@student.fh-kiel.de>
2021-11-15 22:05:17 +0100

    add water and fertelize buttons

--- lib/plant_overview.dart
+++ lib/plant_overview.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:fluttericon/entypo_icons.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -20,12 +21,12 @@ class _PlantOverviewState extends State<PlantOverview> {
     // map mit name, imag und onPressedFunction
     List<Map> plantList = [
       {
-        'name': 'Zierlicher Peter',
+        'name': 'Zierlicher Peter Zierlicher Peter Zierlicher Peter ',
         'image': GeneralArguments.defaultPlantImg,
         'plantRoute': '/plant'
       },
       {
-        'name': 'Zierlicher Peter',
+        'name': 'Zierlicher Peterpeterpeterpeterpeter',
         'image': GeneralArguments.defaultPlantImg,
         'plantRoute': '/plant'
       },
@@ -64,34 +65,74 @@ class _PlantOverviewState extends State<PlantOverview> {
                   Navigator.pushNamed(context, '/plant');
                 },
                 splashColor: Colors.green,
-                child: Column(
+                child: Stack(
+                  alignment: AlignmentDirectional.topEnd,
                   children: [
-                    Expanded(
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(8),
-                            topRight: Radius.circular(8)),
-                        child: Image(
-                          image: GeneralArguments.imagePath.isEmpty
-                              ? GeneralArguments.defaultPlantImg
-                              : FileImage(File(GeneralArguments.imagePath)),
-                          width: double.infinity,
-                          fit: BoxFit.cover,
+                    Column(
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(8),
+                                topRight: Radius.circular(8)),
+                            child: Image(
+                              image: GeneralArguments.imagePath.isEmpty
+                                  ? GeneralArguments.defaultPlantImg
+                                  : FileImage(File(GeneralArguments.imagePath)),
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                      ),
+                        Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade700,
+                              borderRadius: const BorderRadius.only(
+                                bottomLeft: Radius.circular(8),
+                                bottomRight: Radius.circular(8),
+                              )),
+                          child: Text(
+                            plantList[index]['name'],
+                            textAlign: TextAlign.center,
+                            maxLines: 1,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    Container(
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Colors.green.shade800,
-                          borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(8),
-                            bottomRight: Radius.circular(8),
-                          )),
-                      child: Text(
-                        plantList[index]['name'],
-                        textAlign: TextAlign.center,
-                      ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: const Icon(
+                              Entypo.droplet,
+                              size: 20,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: const CircleBorder(),
+                              padding: const EdgeInsets.all(10),
+                              primary: Colors.blue,
+                              minimumSize: Size.zero,
+                            )),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Icon(
+                            Entypo.leaf,
+                            size: 20,
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(10),
+                            primary: Colors.deepOrange,
+                            minimumSize: Size.zero,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
