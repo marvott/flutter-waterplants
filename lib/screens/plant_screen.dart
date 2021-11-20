e36a8efb1cc7d4568ee682fe4dd26949cd7a549d
david.weissensteiner <david.weissensteiner@student.fh-kiel.de>
2021-11-20 17:44:42 +0100

    add possibility to change plant names

--- lib/screens/plant_screen.dart
+++ lib/screens/plant_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/general.dart';
 import 'package:flutter_application_1/models/plant.dart';
+import 'package:fluttericon/entypo_icons.dart';
 
 class PlantScreen extends StatefulWidget {
   final Function callback;
@@ -18,142 +19,29 @@ class PlantScreen extends StatefulWidget {
 }
 
 class _PlantScreenState extends State<PlantScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  //Farben der Listview, gehe sicher dass es die RICHTIGE LÄNGE HAT!
+  final List myColors = <Color>[
+    Colors.transparent,
+    Colors.grey.shade700,
+    Colors.grey.shade700,
+    Colors.grey.shade700,
+  ];
+
   @override
   Widget build(BuildContext context) {
-    //TODO: kann ich das vor dem Build platzieren UND die Properties vom StatefulWidget kriegen?
-    //Elemente die in der Liestview sind
-    final List<Widget> plantElements = [
-      ClipRRect(
-          borderRadius: const BorderRadius.all(Radius.circular(8)),
-          child: Image(
-            image: widget.plantProperties.imagePath.isEmpty
-                ? GeneralArguments.defaultPlantImg
-                : FileImage(File(widget.plantProperties.imagePath)),
-            height: 400,
-            fit: BoxFit.fitWidth,
-          )),
-      Row(
-        children: [
-          Expanded(
-            child: Text.rich(
-              TextSpan(
-                  text: widget.plantProperties.name + '\n',
-                  style: const TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: widget.plantProperties.roomName,
-                        style: const TextStyle(
-                            fontSize: 20, color: Colors.white70))
-                  ]),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Icon(Icons.edit),
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.all(15),
-            ),
-          )
-        ],
-      ),
-      Row(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              child: Text.rich(
-                TextSpan(
-                    text: 'Gießen\n',
-                    style: const TextStyle(color: Colors.blue),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: widget.plantProperties.waterInDays(),
-                        style: const TextStyle(color: Colors.white),
-                      )
-                    ]),
-              ),
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.all(Radius.circular(8)),
-                color: Colors.grey.shade700,
-                border: Border.all(
-                  width: 8,
-                  color: Colors.transparent,
-                ),
-              ),
-            ),
-          ),
-          //TODO: sehr hässliche Abfrage, das geht schöner!
-          widget.plantProperties.fertilising == null
-              ? const SizedBox.shrink()
-              : Expanded(
-                  child: Row(
-                    children: [
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Expanded(
-                        child: Container(
-                          child: const Text.rich(
-                            TextSpan(
-                                text: 'Düngen\n',
-                                style: TextStyle(color: Colors.deepOrange),
-                                children: <TextSpan>[
-                                  TextSpan(
-                                    text: 'In 14 Tagen',
-                                    style: TextStyle(color: Colors.white),
-                                  )
-                                ]),
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(8)),
-                            color: Colors.grey.shade700,
-                            border: Border.all(
-                              width: 8,
-                              color: Colors.transparent,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-        ],
-      ),
-      Text.rich(
-        TextSpan(
-            text: 'Notizen\n',
-            style: const TextStyle(color: Colors.indigo),
-            children: <TextSpan>[
-              TextSpan(
-                text: widget.plantProperties.notes,
-                style: const TextStyle(color: Colors.white),
-              )
-            ]),
-      ),
-    ];
-    //Farben der Listview
-    final List myColors = <Color>[
-      Colors.transparent,
-      Colors.grey.shade700,
-      Colors.grey.shade700,
-      Colors.grey.shade700,
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.plantProperties.name),
       ),
       body: ListView.separated(
         padding: const EdgeInsets.all(8),
-        itemCount: plantElements.length,
+        itemCount: myColors.length,
         // ListView.separated muss mit dem itemBuilder gebaut werden
         itemBuilder: (BuildContext context, int index) {
           return Container(
-              child: plantElements[index],
+              child: buildPlantScreenElements(context, index),
               // das Bild (Index 0) ist schon in einem abgerundeten Container
               decoration: index == 0 || index == 2
                   ? null
@@ -180,8 +68,6 @@ class _PlantScreenState extends State<PlantScreen> {
             Navigator.pushNamed(context, '/camera')
                 .then((imagePath) => setState(() {
                       widget.plantProperties.setImagePath = imagePath;
-                      //TODO: Diese Zeile löschen, Fotografieren soll nicht den Namen ändern
-                      widget.plantProperties.setName = "Fotografierter Peter";
                       widget.callback();
                     }));
           }
@@ -191,4 +77,161 @@ class _PlantScreenState extends State<PlantScreen> {
       floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
     );
   }
+
+  void showBottomSheet(BuildContext context) => showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Form(
+            key: formKey,
+            child: Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      final isValid = formKey.currentState!.validate();
+                      if (isValid) {
+                        formKey.currentState!.save();
+                        setState(() {
+                          widget.callback();
+                        });
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: const Text("Fertig")),
+                TextFormField(
+                    initialValue: widget.plantProperties.name,
+                    decoration: const InputDecoration(
+                      icon: Icon(Entypo.droplet),
+                      border: OutlineInputBorder(),
+                      labelText: "Name",
+                    ),
+                    onSaved: (String? value) =>
+                        widget.plantProperties.setName = value!,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (String? value) {
+                      return (value == null || value.isEmpty)
+                          ? 'Darf nicht leer sein'
+                          : null;
+                    }),
+              ],
+            ),
+          );
+        },
+      );
+
+  Widget buildPlantScreenElements(BuildContext context, int index) => <Widget>[
+        //Elemente die in der Liestview sind
+        ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            child: Image(
+              image: widget.plantProperties.imagePath.isEmpty
+                  ? GeneralArguments.defaultPlantImg
+                  : FileImage(File(widget.plantProperties.imagePath)),
+              height: 400,
+              fit: BoxFit.fitWidth,
+            )),
+        Row(
+          children: [
+            Expanded(
+              child: Text.rich(
+                TextSpan(
+                    text: widget.plantProperties.name + '\n',
+                    style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: widget.plantProperties.roomName,
+                          style: const TextStyle(
+                              fontSize: 20, color: Colors.white70))
+                    ]),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                showBottomSheet(context);
+              },
+              child: const Icon(Icons.edit),
+              style: ElevatedButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(15),
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Text.rich(
+                  TextSpan(
+                      text: 'Gießen\n',
+                      style: const TextStyle(color: Colors.blue),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: widget.plantProperties.waterInDays(),
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ]),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(Radius.circular(8)),
+                  color: Colors.grey.shade700,
+                  border: Border.all(
+                    width: 8,
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            //TODO: sehr hässliche Abfrage, das geht schöner!
+            widget.plantProperties.fertilising == null
+                ? const SizedBox.shrink()
+                : Expanded(
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: Container(
+                            child: const Text.rich(
+                              TextSpan(
+                                  text: 'Düngen\n',
+                                  style: TextStyle(color: Colors.deepOrange),
+                                  children: <TextSpan>[
+                                    TextSpan(
+                                      text: 'In 14 Tagen',
+                                      style: TextStyle(color: Colors.white),
+                                    )
+                                  ]),
+                            ),
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(8)),
+                              color: Colors.grey.shade700,
+                              border: Border.all(
+                                width: 8,
+                                color: Colors.transparent,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+          ],
+        ),
+        Text.rich(
+          TextSpan(
+              text: 'Notizen\n',
+              style: const TextStyle(color: Colors.indigo),
+              children: <TextSpan>[
+                TextSpan(
+                  text: widget.plantProperties.notes,
+                  style: const TextStyle(color: Colors.white),
+                )
+              ]),
+        ),
+      ][index];
 }
