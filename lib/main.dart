67592b2da3ef86f352ee0029ea00f72dd2318d0a
david.weissensteiner <david.weissensteiner@student.fh-kiel.de>
2021-12-10 10:02:44 +0100

    fixing WEIRD firebase build errors

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ import 'screens/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
-  // Kamera initialisieren
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); //TODO hinzugefügt
   final cameras = await availableCameras();
