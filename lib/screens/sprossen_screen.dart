531c858420c056c70e42630c64fdcbc33f1688cc
Marvin Ottersberg <marvin.ottersberg@student.fh-kiel.de>
2021-12-14 16:06:02 +0100

    Abfrage aus Firestore funktioniert

--- lib/screens/sprossen_screen.dart
+++ lib/screens/sprossen_screen.dart
@@ -1,4 +1,7 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:intl/intl.dart';
+
+import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +21,9 @@ class _SprossenRouteState extends State<SprossenRoute> {
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference itemsRef =
+        FirebaseFirestore.instance.collection('sprossen');
+    Query query = itemsRef.orderBy('name');
     //UI
     return Scaffold(
       appBar: AppBar(
@@ -27,7 +33,132 @@ class _SprossenRouteState extends State<SprossenRoute> {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          //First Row from Table = Header
+          Expanded(
+              child: SizedBox(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: query.snapshots(),
+                builder: (BuildContext context, var snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  List<Item> items = snapshot.data!.docs
+                      .map((doc) => Item.fromJson(
+                          (doc.data() as Map<String, dynamic>)
+                            ..['id'] = doc.id))
+                      .toList();
+                  return ListView(children: _listTiles(itemsRef, items));
+                }),
+          )),
+          ElevatedButton(
+              onPressed: () => _addItem(itemsRef), child: Icon(Icons.add)),
+
+          //
+        ],
+      )),
+    );
+  }
+}
+
+//Versuch einzelne Werte zu bekommen -> Noch behalten und ausprobieren. Am Ende löschen
+Future<Object?> getFirestoreValues() async {
+  CollectionReference collRef = firestore.collection('sprossen');
+  //DocumentReference docRef = collRef.doc('Alfalfa');
+  var docSnapshot = await collRef.doc('Alfalfa').get();
+  var test = await collRef.doc('Alfalfa').snapshots();
+  print(test);
+
+  Map<String, dynamic>? data = docSnapshot.data() as Map<String, dynamic>?;
+  // You can then retrieve the value from the Map like this:
+  var value = data?['Keimdauer'];
+  print("value: " + value);
+
+  QuerySnapshot query = await collRef.get();
+  List sprossenlist = [];
+  query.docs.forEach((document) => sprossenlist.add(document.data()));
+  //print("Liste " + sprossenlist[0].toString());
+  print(sprossenlist);
+
+  return sprossenlist;
+}
+
+List<Widget>? itemsStreamBuilder() {
+  CollectionReference itemsRef =
+      FirebaseFirestore.instance.collection('sprossen');
+  Query query = itemsRef.orderBy('name');
+  StreamBuilder<QuerySnapshot>(
+      stream: query.snapshots(),
+      builder: (BuildContext context, var snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        List<Item> items = snapshot.data!.docs
+            .map((doc) => Item.fromJson(
+                (doc.data() as Map<String, dynamic>)..['id'] = doc.id))
+            .toList();
+        return ListView(children: _listTiles(itemsRef, items));
+      });
+}
+
+_addItem(CollectionReference itemsRef) {
+  final name = "test";
+  final keimdauer = '4';
+
+  final item = Item(name, keimdauer);
+  itemsRef
+      .add(item.toJson())
+      .then((doc) => print('Added a new item with id = ${doc.id}'));
+}
+
+_deleteItem(CollectionReference itemsRef, String id) {
+  itemsRef.doc(id).delete().then((_) => print('Deleted item with id = $id'));
+}
+
+_listTiles(CollectionReference itemsRef, List<Item> items) {
+  return items
+      .map((i) => ListTile(
+          title: Text(i.name,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+          subtitle: Text("Keimdauer (Tage):" + i.keimdauer),
+          leading: Container(width: 40, height: 40, color: Colors.blue),
+          trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _deleteItem(itemsRef, i.id))))
+      .toList();
+}
+
+class Item {
+  late String id;
+  String name;
+  String keimdauer;
+  //Color color;
+  //DateTime created = DateTime.now();
+
+  Item(this.name, this.keimdauer);
+
+  Item.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        keimdauer = json['Keimdauer (Tage)'];
+
+  Map<String, dynamic> toJson() =>
+      {'name': name, 'Keimdauer (Tage)': keimdauer};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* ALTE IDEEN ABER ERSTMAL NOCH ALS BACKUP UND REFERENZ
+*
+//First Row from Table = Header
           Padding(
               padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
               child: Row(
@@ -48,74 +179,7 @@ class _SprossenRouteState extends State<SprossenRoute> {
                           fontSize: 20, fontWeight: FontWeight.bold)))
                 ],
               )),
-          //Line to set Header apart from other Text
-          const Divider(
-            height: 5,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          //Second Row
-          Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text.rich(TextSpan(
-                      text: 'Saaten', style: TextStyle(fontSize: fontsize))),
-                  Text.rich(TextSpan(
-                      text: 'Menge', style: TextStyle(fontSize: fontsize))),
-                  Text.rich(TextSpan(
-                      text: 'Keimdauer', style: TextStyle(fontSize: fontsize)))
-                ],
-              )),
-          FloatingActionButton(
-              child: Text('Klick'), onPressed: () => getFirestoreValues()),
-        ],
-      )),
-    );
-  }
-}
-
-Future<Object> getFirestoreValues() async {
-  CollectionReference collRef = firestore.collection('sprossen');
-  //DocumentReference docRef = collRef.doc('Alfalfa');
-  var docSnapshot = await collRef.doc('Alfalfa').get();
-  var test = await collRef.doc('Alfalfa').snapshots();
-  print(test);
-
-  print("Teiler\n");
-  Map<String, dynamic>? data = docSnapshot.data() as Map<String, dynamic>?;
-  // You can then retrieve the value from the Map like this:
-  var value = data?['Keimdauer'];
-  print(value);
-
-  QuerySnapshot query = await collRef.get();
-  List sprossenlist = [];
-  query.docs.forEach((doc) => sprossenlist.add(doc.data()));
-
-  //print("Liste " + sprossenlist[0].toString());
-  //print(sprossenlist);
-  streamding();
-  return sprossenlist;
-}
-
-Widget streamding() {
-  return StreamBuilder(
-      stream: firestore.collection('sprossen').doc('Alfalfa').snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Text("Loading");
-        }
-        var userDocument = snapshot.data;
-        print(userDocument);
-        return Text(userDocument.toString());
-      });
-}
-
-
-/*ElevatedButton(
+ElevatedButton(
                 onPressed: () {
                   Navigator.pop(context);
                   // Navigate back to first route when tapped.
