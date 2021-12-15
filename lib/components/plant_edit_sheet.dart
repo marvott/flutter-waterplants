a9a348bcfbd5e84ea3f482a0111ee908cfebd390
david.weissensteiner <david.weissensteiner@student.fh-kiel.de>
2021-12-15 16:54:56 +0100

    edit layout of bottomsheet

--- lib/components/plant_edit_sheet.dart
+++ lib/components/plant_edit_sheet.dart
@@ -17,76 +17,97 @@ showBottomSheetPlantEdit(BuildContext context, Plant plant, Function callback,
         ),
         child: Form(
           key: formKeyPlantedit,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    final isValid = formKeyPlantedit.currentState!.validate();
-                    if (isValid) {
-                      formKeyPlantedit.currentState!.save();
-                      // setState(() {
-                      //   widget.plantScreenOverviewCallback();
-                      // });
-                      callback();
-                      plantOverviewCallback();
-                      Navigator.pop(context);
-                    }
-                  },
-                  child: const Text("Speichern")),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                    initialValue: plant.name,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Name",
-                      icon: Icon(RpgAwesome.wooden_sign),
+          child: Padding(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Bearbeiten:",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            final isValid =
+                                formKeyPlantedit.currentState!.validate();
+                            if (isValid) {
+                              formKeyPlantedit.currentState!.save();
+                              callback();
+                              Navigator.pop(context);
+                            }
+                          },
+                          child: const Text(
+                            "Speichern",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ],
                     ),
-                    onSaved: (String? value) => plant.setName = value!,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (String? value) {
-                      return (value == null || value.isEmpty)
-                          ? 'Darf nicht leer sein'
-                          : null;
-                    }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                        initialValue: plant.name,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Name",
+                          icon: Icon(RpgAwesome.wooden_sign),
+                        ),
+                        onSaved: (String? value) => plant.setName = value!,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (String? value) {
+                          return (value == null || value.isEmpty)
+                              ? 'Darf nicht leer sein'
+                              : null;
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                        initialValue: plant.species,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Spezies",
+                          icon: Icon(RpgAwesome.flowers),
+                        ),
+                        onSaved: (String? value) => plant.setSpecies = value!,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (String? value) {
+                          return (value == null || value.isEmpty)
+                              ? 'Darf nicht leer sein'
+                              : null;
+                        }),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: TextFormField(
+                        initialValue: plant.roomName,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Zimmer",
+                          icon: Icon(FontAwesome5.house_user),
+                        ),
+                        onSaved: (String? value) => plant.setRoomName = value!,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (String? value) {
+                          return (value == null || value.isEmpty)
+                              ? 'Darf nicht leer sein'
+                              : null;
+                        }),
+                  ),
+                  const SizedBox(height: 50)
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                    initialValue: plant.species,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Spezies",
-                      icon: Icon(RpgAwesome.flowers),
-                    ),
-                    onSaved: (String? value) => plant.setSpecies = value!,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (String? value) {
-                      return (value == null || value.isEmpty)
-                          ? 'Darf nicht leer sein'
-                          : null;
-                    }),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: TextFormField(
-                    initialValue: plant.roomName,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Zimmer",
-                      icon: Icon(FontAwesome5.house_user),
-                    ),
-                    onSaved: (String? value) => plant.setRoomName = value!,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (String? value) {
-                      return (value == null || value.isEmpty)
-                          ? 'Darf nicht leer sein'
-                          : null;
-                    }),
-              ),
-              const SizedBox(height: 50)
-            ],
+            ),
           ),
         ),
       );
