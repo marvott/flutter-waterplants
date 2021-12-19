ec13283f3cd96625f62ebe85cd04e74def1676f4
david.weissensteiner <david.weissensteiner@student.fh-kiel.de>
2021-12-19 12:43:49 +0100

    remove deleted import

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_application_1/components/no_user_widget.dart';
 
 import 'plant_overview_screen.dart';
 import 'settings_screen.dart';
